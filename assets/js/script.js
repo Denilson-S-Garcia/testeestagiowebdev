4c81c14a528997af4f3320abfbb1aa17ddb476b1
Denilson-S-Garcia <denilsonsg2017@gmail.com>
2022-05-27 14:54:07 -0300

    atualização e melhorias

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function menumobile(){
+    let navon = document.querySelector('ul')
+   // let menu = document.querySelector('.menu-opener')
+   // menu.addEventListener('click', menumobile)
+   if (navon.style.display == 'flex'){
+     navon.style.display = 'none'
+   }else {
+     navon.style.display = 'flex'
+   }
+    
+  
+    
+ }
